62943f34e7d04f741ee01d6d4a30d9cea140030f
ericlu <ericlu9256@gmail.com>
2023-10-17 20:54:25 -0700

    fixed not getting uid error

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
-import { getFireStore, getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
